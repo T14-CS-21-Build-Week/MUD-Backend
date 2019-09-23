6135d68be0829296667d5d7323dcaa43f2d210e0
mjfalahee <mjfalahee@gmail.com>
2019-09-23 14:27:53 -0700

    heroku database is deployed and storing information properly

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -101,15 +101,10 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # },
-    # 'heroku': {
-
-    # }
 }
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-#Database url
-# dj_database_url.config(default=config('DATABASE_URL'))
 
 
 # Password validation
