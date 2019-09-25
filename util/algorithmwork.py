874907a18f9f0d97562a16586b2eb14bcc7b2f07
mjfalahee <mjfalahee@gmail.com>
2019-09-25 12:28:34 -0700

    Working on random room descriptions/titles

--- util/algorithmwork.py
+++ util/algorithmwork.py
@@ -42,7 +42,10 @@ from django.db import models
 #         Connect two rooms in the given n/s/e/w direction
 #         '''
 #         return getattr(self, f"{direction}_to")
-
+title_adj = ["Dank","Dark","Creepy","Shrouded","Old","Uneven","Disheveled","Bloodstained","Unkempt","Cluttered","Gloomy","Lightless","Obscured","Pitch-Black"]
+title_noun = ["Corridor", "Hall","",""]
+description_start = ["",""]
+description_end = ["", ""]
 
 class World:
     def __init__(self):
@@ -151,7 +154,59 @@ class World:
             p.currentRoom=firstroom.id
             p.save()
 
+ # def print_rooms(self):
+    #     '''
+    #     Print the rooms in room_grid in ascii characters.
+    #     '''
 
+    #     # Add top border
+    #     str = "# " * ((3 + self.width * 5) // 2) + "\n"
+
+    #     # The console prints top to bottom but our array is arranged
+    #     # bottom to top.
+    #     #
+    #     # We reverse it so it draws in the right direction.
+    #     reverse_grid = list(self.grid) # make a copy of the list
+    #     reverse_grid.reverse()
+    #     for row in reverse_grid:
+    #         # PRINT NORTH CONNECTION ROW
+    #         str += "#"
+    #         for room in row:
+    #             if room is not None and room.n_to is not None:
+    #                 str += "  |  "
+    #             else:
+    #                 str += "     "
+    #         str += "#\n"
+    #         # PRINT ROOM ROW
+    #         str += "#"
+    #         for room in row:
+    #             if room is not None and room.w_to is not None:
+    #                 str += "-"
+    #             else:
+    #                 str += " "
+    #             if room is not None:
+    #                 str += f"{room.id}".zfill(3)
+    #             else:
+    #                 str += "   "
+    #             if room is not None and room.e_to is not None:
+    #                 str += "-"
+    #             else:
+    #                 str += " "
+    #         str += "#\n"
+    #         # PRINT SOUTH CONNECTION ROW
+    #         str += "#"
+    #         for room in row:
+    #             if room is not None and room.s_to is not None:
+    #                 str += "  |  "
+    #             else:
+    #                 str += "     "
+    #         str += "#\n"
+
+    #     # Add bottom border
+    #     str += "# " * ((3 + self.width * 5) // 2) + "\n"
+
+    #     # Print string
+    #     print(str)
    
 
 w=World()
